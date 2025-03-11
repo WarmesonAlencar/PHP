a61db7c006cc3e86060d5508dd48e52a9a917816
Warmeson de Freitas Alencar <warmisonalencar@gmail.com>
2025-03-11 20:50:15 -0300

    teste de apagar comentário

--- calculo.php
+++ calculo.php
@@ -6,5 +6,7 @@ $media = $_POST['media'];
 echo "<h1> Informações do aluno</h1>";
 echo ("O Aluno <b>$nome</b> ficou com a media: <font color='blue'> $media");
 
+// apaguei o teste
+
 ?>
-Alteração feita no vs code
+
