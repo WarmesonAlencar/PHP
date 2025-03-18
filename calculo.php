ae25c5105ada84c18ed68f1375f18dc96d41b1ed
Warmeson de Freitas Alencar <warmisonalencar@gmail.com>
2025-03-18 20:22:21 -0300

    refazendo os códigos

--- calculo.php
+++ calculo.php
@@ -1,10 +1,22 @@
 <?php
 
 $nome = $_POST['nome'];
-$media = $_POST['media'];
+$sobrenome = $_POST['sobrenome'];
+$nota1 = $_POST['nota1'];
+$nota2 = $_POST['nota2'];
+$nota3 = $_POST['nota3'];
 
-echo "<h1> Informações do aluno</h1>";
-echo ("O Aluno <b>$nome</b> ficou com a media: <font color='blue'> $media");
+$media = ($nota1 + $nota2 + $nota3) / 3;
+echo "A média é: $media<br>";
+
+if($media >= 6) {
+    echo"O aluno $nome $sobrenome está <font color='green'> Aprovado</font>";
+}
+elseif ($media >= 4 ) {
+    echo" O Aluno está de recuperação";
+}
+
+
 
 // apaguei o teste
 
--- form.php
+++ form.php
@@ -1,9 +1,18 @@
 <?php
-$valor = $_POST["v1"];
 
-$resultado = $valor * $valor;
-echo("<h3>Calculando o conteúdo da variável<br><br></h3>");
+$nome = $_POST['nome'];
+$nota1 = $_POST['n1'];
+$nota2 = $_POST['n2'];
+$nota3 = $_POST['n3'];
 
-echo(" O valor ao quadrado é :<font color='red'>$resultado");
+$media = ($nota1+$nota2+$nota3)/3;
+echo ("O aluno ficou com média: $media");
 
+if ($media>=6){
+    echo("Aluno <font color='green'> APROVADO!</font>");
+}elseif ($media > 4 || $media < 6){
+    echo ("Aluno <font color='azul'> RECUPERAÇÃO</font>");
+}else {
+    echo ("Aluno <font color='red'> REPROVADO</font>");
+}
 ?>
