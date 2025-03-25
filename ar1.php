49c2ca5d2aceb031058365533d1fed81732cfa7e
Warmeson de Freitas Alencar <warmisonalencar@gmail.com>
2025-03-25 20:08:44 -0300

    commit pelo vscode

--- ar1.php
+++ ar1.php
@@ -2,5 +2,10 @@
 
 $array = array("Terça-feira", "Quarta-feira", "Quinta");
 print_r($array);
+echo "<br>";
+echo($array[1]);
+echo"<hr>";
+$array2 = ["aula","php","html","CSS"];
+print_r($array);
 
 ?>
