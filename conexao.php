8eb51107c7fbc00bbc4ef4a05893c543ab7e92d3
Warmeson de Freitas Alencar <warmisonalencar@gmail.com>
2025-04-08 19:36:41 -0300

    mudamças ultima aula

---
+++ conexao.php
@@ -0,0 +1,10 @@
+<?php
+
+define ('HOST','localhost');
+define ('USER','root');
+define ('PASS','');
+define('PORT', '3306');
+define ('BASE','cadastro');
+
+$conn = new mysqli(HOST, USER, PASS, BASE);
+
