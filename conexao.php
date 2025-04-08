52206e6d37b09768f8eb1cfa95b97777efcda068
Warmeson de Freitas Alencar <warmisonalencar@gmail.com>
2025-04-08 19:45:21 -0300

    procurando o erro

--- conexao.php
+++ conexao.php
@@ -3,8 +3,7 @@
 define ('HOST','localhost');
 define ('USER','root');
 define ('PASS','');
-define('PORT', '3306');
 define ('BASE','cadastro');
 
-$conn = new mysqli(HOST, USER, PASS, BASE);
+$conn = new mysqli(HOST, USER, PASS, BASE, 3306);
 
