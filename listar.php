7486c7d42812129b7ff60d0fd82d4d90534b3f6d
Warmeson de Freitas Alencar <warmisonalencar@gmail.com>
2025-04-08 21:44:33 -0300

    continua...

--- listar.php
+++ listar.php
@@ -11,7 +11,36 @@
 include('conexão.php');
 
 print("<h1> Listagem de Usuários</h1>");
-$sql = 'select * from '
+$sql = 'select * from usuarios';
+$res = $conn->query($sql);
+
+$qtde = $res ->num_rows;
+if($qtde >0){
+    print("<table>");
+    print("<tr>");
+    print("<th> Código</th>");
+    print("<th> Nome</th>");
+    print("<th> Senha</th>");
+    print("<th> CPF</th>");
+    print("<th> Data</th>");
+    print("</tr>");
+}while($row = $res->fetch_object()){
+    
+    print("<tr>");
+    print("<td>".$row->$cod."</td>");
+    print("<td>".$row->$nome."</td>");
+    print("<td>".$row->$senha."</td>");
+    print("<td>".$row->$email."</td>");
+    print("<td>".$row->$cpf."</td>");
+    print("<td>".$row->$data."</td>");
+    
+    print("</tr>");
+    print("</table>");
+}
+else{
+    print("<p> Não foram encontrados registros! </p>");
+}
+
 
 ?>
 
