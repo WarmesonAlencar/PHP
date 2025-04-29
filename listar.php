c82d244ec8df88848333c29a1d7e71eaacde8aef
Warmeson de Freitas Alencar <warmisonalencar@gmail.com>
2025-04-29 19:46:37 -0300

    aula_29.04.2025

--- listar.php
+++ listar.php
@@ -1,48 +1,46 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista dos Banco de Dados</title>
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+<div>
 <?php
 
-include('conexão.php');
 
+include('conexao.php');
 print("<h1> Listagem de Usuários</h1>");
 $sql = 'select * from usuarios';
 $res = $conn->query($sql);
-
 $qtde = $res ->num_rows;
 if($qtde >0){
-    print("<table>");
-    print("<tr>");
+    print("<table class='table table-dark striped-columns'>");
+    print ("<tr>");
     print("<th> Código</th>");
     print("<th> Nome</th>");
     print("<th> Senha</th>");
+    print("<th> Email</th>");
     print("<th> CPF</th>");
     print("<th> Data</th>");
-    print("</tr>");
-}while($row = $res->fetch_object()){
-    
-    print("<tr>");
-    print("<td>".$row->$codigo."</td>");
-    print("<td>".$row->$nome."</td>");
-    print("<td>".$row->$senha."</td>");
-    print("<td>".$row->$email."</td>");
-    print("<td>".$row->$cpf."</td>");
-    print("<td>".$row->$data."</td>");
-    
-    print("</tr>");
-    print("</table>");
+    print ("</tr>");
+while($row = $res->fetch_object()){
+    print "<tr>";
+    print"<td>".$row ->codigo."</td>";
+    print"<td>".$row ->nome."</td>";
+    print"<td>".$row ->senha."</td>";
+    print"<td>".$row ->email."</td>";
+    print"<td>".$row ->cpf."</td>";
+    print"<td>".$row ->data."</td>";
+    print "</tr>"; 
 }
-else{
-    print("<p> Não foram encontrados registros! </p>");
-}
+    print"</table>";}
 
 
-?>
-
+?>  
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
