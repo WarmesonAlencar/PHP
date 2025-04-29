44d05a31c72ae85c9fbefee54cf3bbb55dab35d1
Warmeson de Freitas Alencar <warmisonalencar@gmail.com>
2025-04-29 20:06:35 -0300

    codigo atualizado

--- editar.php
+++ editar.php
@@ -1,74 +1,79 @@
 <?php
-include_once("conexao.php")
+include_once("conexão.php");
 $codigo = $_GET['codigo'];
 
 $sql = "Select * from usuarios where codigo=$codigo";
+$res = $conn -> query($sql);
 
-$res = $conn
-
-
+if ($res==true){
+    while($row = mysqli_fetch_assoc($res))
+    {
+        $nome = $_POST['nome'];
+        $senha = $_POST['senha'];
+        $email = $_POST['email'];
+        $cpf = $_POST['cpf'];
+        $data = $_POST['data'];
+    }
+}  else {
+    print("<script>alert('Dados não encontrados')</script>");
+}
 
 ?>
-
-
-
 
 <!doctype html>
 <html lang="pt-br">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PHP</title>
+    <title>php</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
     
-    
-
     <div class="container">
-        <h1>Cadastro de Usuário</h1>
-        <hr>
-    <form class="row g-3" action="cadastrar.php" method="post">
-
-        <div class="col-12">
-            <label for="inputAddress" class="form-label">Nome</label>
-            <input type="text" class="form-control" name="nome" placeholder="Digite seu nome">
-          </div>
-
-         <div class="col-md-6">
-          <label for="inputPassword4" class="form-label">Senha</label>
-          <input type="password" class="form-control" name="senha">
-        </div> 
-
-        <div class="col-md-6">
-          <label for="inputEmail4" class="form-label">Email</label>
-          <input type="email" class="form-control" name="email">
-        </div>
+        <h1>Atualizar Dades de Usuário</h1>
+    <hr>
+    <form class="row g-3" action="EditarSalvar.php" method="post">
         
         <div class="col-12">
+            <label for="inputAddress" class="form-label">Nome</label>
+            <input type="text" class="form-control" name="nome" value="<?php echo $nome ?>">
+          </div>
+
+          <div class="col-md-6">
+            <label for="inputPassword4" class="form-label">Senha</label>
+            <input type="password" class="form-control" name="senha" value="<?php echo $senha ?>">
+          </div>
+                
+        <div class="col-md-6">
+          <label for="inputEmail4" class="form-label">Email</label>
+          <input type="email" class="form-control" name="email" value="<?php echo $email ?>">
+        </div>
+                
+        <div class="col-12">
           <label for="inputAddress2" class="form-label">CPF</label>
-          <input type="text" class="form-control" name="cpf" placeholder="Digite seu cpf">
+          <input type="text" class="form-control" name="cpf" value="<?php echo $cpf ?>">
         </div>
         <div class="col-md-6">
           <label for="inputCity" class="form-label">Data</label>
-          <input type="date" class="form-control" name="data">
+          <input type="date" class="form-control" name="data" value="<?php echo $data ?>">
         </div>
-        
-        
+       
+       
         <div class="col-12">
           <div class="form-check">
             <input class="form-check-input" type="checkbox" id="gridCheck">
             <label class="form-check-label" for="gridCheck">
-              Concordo com os termos
+              Concordo com os termos.
             </label>
           </div>
         </div>
         <div class="col-12">
-          <button type="submit" class="btn btn-primary">Cadastrar</button>
+          <input type="submit" class="btn btn-primary" id="update" name="update" value="Atualizar">
+          <input type="hidden" name="cod" id="cod" value="<?php echo $codigo ?>">
         </div>
       </form>
     </div>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
